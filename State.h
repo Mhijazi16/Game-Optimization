a796f655b4520938a1fb6e18139dc7ea91a28273
Mhijazi16 <gultimate0@gmail.com>
2024-05-27 10:12:41 +0300

    State : created State struct with all needed fields

--- State.h
+++ State.h
@@ -3,3 +3,14 @@
 #include <iostream>
 
 using namespace std;
+
+struct State{ 
+  vector<vector<string>> Map;
+  Point Player; 
+  vector<Point> Meat;
+  vector<Point> Boxes; 
+  vector<Point> Bones; 
+  string Action = "NULL";
+  int Rows = 0; 
+  int Cols = 0;
+};
